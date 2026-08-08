7c8793ab1e3d102f9239d94a6a0d155f9fdc7196
Yusuf Özkan <ozkany616@gmail.com>
2026-08-08 19:56:13 +0300

    I am gonna extend this project

---
+++ models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import  Column , Integer , String , Boolean
+from database import Base
+
+class TodoModel(Base):
+    __tablename__ = "todos" 
+    id = Column(Integer, primary_key=True,index=True)
+    title = Column(String,index=True, nullable=True)
+    description= Column(Booelan, default=True)
