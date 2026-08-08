e62e14b73152b389f1785966bcb8a70187b6be80
Yusuf Özkan <ozkany616@gmail.com>
2026-08-08 19:57:13 +0300

    go

--- models.py
+++ models.py
@@ -5,4 +5,5 @@ class TodoModel(Base):
     __tablename__ = "todos" 
     id = Column(Integer, primary_key=True,index=True)
     title = Column(String,index=True, nullable=True)
-    description= Column(Booelan, default=True)
+    description= Column(String,index=True, nullable=True)
+    completed= Column(Boolean,default=False)
